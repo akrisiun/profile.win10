8ecccad6dec9cf9a760463c4deba944cad5d29c2
AndriusK <akrisiun@gmail.com>
2017-02-16 09:21:43 +0200

    Phx-FatalError MSBUILD fix : regsvr32 msdia140.dll

---
+++ reg-PhxFatal.cmd
@@ -0,0 +1,8 @@
+@REM http://larrynung.github.io/2016/11/24/Visual-Studio-Exception-of-type-Phx-FatalError-was-thrown/
+
+C: 
+@rem CD c:\Program Files (x86)\Microsoft Visual Studio 14.0\Team Tools\Performance Tools\x64\
+CD c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\
+regsvr32 msdia140.dll 
+
+@Pause
